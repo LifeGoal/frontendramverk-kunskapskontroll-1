ac4413ee97e545aed25f89c68e8d5fca037a3c02
Viktor Lindqvist <lindqvistcs@gmail.com>
2026-05-04 14:45:47 +0200

    Finalized champion list (might add more features later)

---
+++ src/components/list/ChampionList.jsx
@@ -0,0 +1,13 @@
+import Champion from './ChampionItem';
+
+function ChampionList({ champions }) {
+    return (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 lg:gap-8 mt-16 w-full">
+            {champions.sort((a, b) => a.name.localeCompare(b.name, "sv")).map(champion => (
+                <Champion key={champion.id} champion={champion} />
+            ))}
+        </ul>
+    );
+}
+
+export default ChampionList;
